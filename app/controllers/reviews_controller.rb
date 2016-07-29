0113f29c0cf8a6523d8b38f9592567ea86d60939
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-29 20:53:40 +0800

    added code for deleting reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 before_action :require_login, only: [:create, :destroy]
+before_action :correct_review, only: [:destroy]
 
 def create
   @review = current_user.reviews.build(review_params)
@@ -12,12 +13,33 @@ def create
 end
 
 def destroy
+  @review.destroy
+    flash[:success] = "Review deleted"
+    redirect_to current_user
 end
 
 private
+def set_review
+  @review = review.find(params[:id])
+end
 
 def review_params
   params.require(:review).permit(:content, :user_id)
+end
+
+def correct_review
+    @review = Review.find(params[:id])
+    unless (current_user[:id] == @review.user_id)
+    flash[:danger] = "This is not your login. Do you want to login again?"
+    redirect_to current_user
+  end
+end
+
+def require_logout
+  if logged_in?
+  flash[:warning] = "You must be logged out to create a new review"
+  redirect_to(root_url)
+end
 end
 
 end
